6216e85467667e1b70e1c082d96cb9617a1ecba6
Nicolas BOUQUET <nicolas@point-core.com>
2025-01-10 16:06:16 +0100

    [android] Prepare for CodeGen

--- jni/Android.mk
+++ jni/Android.mk
@@ -13,14 +13,23 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := lua
 LOCAL_ARM_MODE := arm
-LOCAL_CFLAGS := -O2 -DLUA_USE_MKSTEMP -std=c++17
-LOCAL_C_INCLUDES += $(addprefix $(LOCAL_PATH)/../,VM/src VM/include Common/Include Compiler/include Ast/include ../libgvfs)
+LOCAL_CFLAGS := -O2 -DLUA_USE_MKSTEMP -std=c++17 -DLUAU_ENABLE_CODEGEN
+LOCAL_C_INCLUDES += $(addprefix $(LOCAL_PATH)/../,VM/src VM/include Common/Include Compiler/include Codegen/include Ast/include ../libgvfs)
 
 LOCAL_SRC_FILES += $(addsuffix .cpp, \
         $(addprefix ../VM/src/,lapi laux lbaselib lbitlib lbuffer lbuflib lbuiltins lcorolib ldblib ldebug ldo lfunc lgc lgcdebug linit lint64lib liolib lmathlib lmem lnumprint lobject loslib lperf lstate lstring lstrlib \
          ltable ltablib ltm ludata lutf8lib lvmexecute lvmload lvmutils) \
         $(addprefix ../Compiler/src/,Builtins BuiltinFolding BytecodeBuilder ConstantFolding Compiler CostModel lcode PseudoCode TableShape Types ValueTracking) \
         $(addprefix ../Ast/src/,Ast Confusables Lexer Location Parser StringUtils TimeTrace))
+
+#CodeGen not yet support for android
+#LOCAL_CFLAGS += -DLUAU_ENABLE_CODEGEN
+#LOCAL_SRC_FILES += $(addsuffix .cpp, \
+        $(addprefix ../CodeGen/src/,BytecodeAnalysis BytecodeSummary CodeAllocator CodeBlockUnwind CodeGen CodeGenAssembly CodeGenContext CodeGenUtils \
+         IrAnalysis IrBuilder IrCallWrapperX64 IrDump IrTranslateBuiltins IrTranslation IrUtils IrValueLocationTracking \
+         lcodegen NativeProtoExecData NativeState OptimizeConstProp OptimizeDeadStore SharedCodeAllocator \
+         AssemblyBuilderX64 OptimizeFinalX64 EmitBuiltinsX64 EmitCommonX64 EmitInstructionX64 CodeGenX64 IrLoweringX64 IrRegAllocX64 UnwindBuilderWin \
+         AssemblyBuilderA64 CodeGenA64 IrLoweringA64 IrRegAllocA64 UnwindBuilderDwarf2))
 
 LOCAL_LDLIBS := -ldl
 LOCAL_SHARED_LIBRARIES := gvfs
